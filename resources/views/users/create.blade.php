3b04582008e3fb9d93393eb08dfbec71dd616ab3
Fathul Muiin <fmuiin14@gmail.com>
2021-01-05 16:34:45 +0700

    283 done validate users create edit then validate category

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -22,45 +22,79 @@ Create New User
         @csrf
 
         <label for="name">Name</label>
-        <input type="text" class="form-control" placeholder="Full Name" name="name" id="name">
+        <input value="{{old('name')}}" type="text" class="form-control {{$errors->first('name') ? "is-invalid" : ""}}" placeholder="Full Name" name="name" id="name">
+        <div class="invalid-feedback">
+            {{$errors->first('name')}}
+        </div>
         <br>
 
         <label for="username">Username</label>
-        <input class="form-control" type="text" placeholder="username" name="username" id="username">
+        <input value="{{old('username')}}" class="form-control {{$errors->first('username') ? "is-invalid" : ""}}" type="text" placeholder="username" name="username" id="username">
+        <div class="invalid-feedback">
+            {{$errors->first('username')}}
+        </div>
         <br>
 
         <label for="roles">Roles</label>
         <br>
-        <input type="checkbox" name="roles[]" id="ADMIN" value="ADMIN">
+        <input class="form-control {{$errors->first('roles') ? "is-invalid" : ""}}" type="checkbox" name="roles[]" id="ADMIN" value="ADMIN">
         <label for="ADMIN">Administrator</label>
 
-        <input type="checkbox" name="roles[]" id="STAFF" value="STAFF">
+        <input class="form-control {{$errors->first('roles') ? "is-invalid" : ""}}" type="checkbox" name="roles[]" id="STAFF" value="STAFF">
         <label for="STAFF">Staff</label>
 
-        <input type="checkbox" name="roles[]" id="CUSTOMER" value="CUSTOMER">
+        <input class="form-control {{$errors->first('roles') ? "is-invalid" : ""}}" type="checkbox" name="roles[]" id="CUSTOMER" value="CUSTOMER">
         <label for="CUSTOMER">Customer</label>
+
+        <div class="invalid-feedback">
+            {{$errors->first('roles')}}
+        </div>
 
         <br><br>
         <label for="phone">Phone Number</label>
         <br>
-        <input type="text" name="phone" class="form-control">
+        <input value="{{old('phone')}}" type="text" name="phone" class="form-control {{$errors->first('phone') ? "is-invalid" : ""}}">
+        <div class="invalid-feedback">
+            {{$errors->first('phone')}}
+        </div>
         <br>
         <label for="address">Address</label>
-        <textarea name="address" id="address" class="form-control"></textarea>
+        <textarea name="address" id="address" class="form-control {{$errors->first('address') ? "is-invalid" : ""}}">{{old('address')}}</textarea>
+        <div class="invalid-feedback">
+            {{$errors->first('address')}}
+        </div>
+
         <br>
         <label for="avatar">Avatar image</label>
         <br>
-        <input type="file" id="avatar" name="avatar" class="form-control">
-        <br>
+        <input type="file" id="avatar" name="avatar" class="form-control {{$errors->first('avatar') ? "is-invalid" : ""}}">
+        <div class="invalid-feedback">
+            {{$errors->first('avatar')}}
+        </div>
+
+        <hr class="my-4">
+
         <label for="email">Email</label>
-        <input class="form-control" placeholder="user@mail.com" type="text" name="email" id="email">
+        <input value="{{old('email')}}" class="form-control {{$errors->first('email') ? "is-invalid" : ""}}" placeholder="user@mail.com" type="text" name="email" id="email">
+        <div class="invalid-feedback">
+            {{$errors->first('email')}}
+        </div>
         <br>
+
         <label for="password">Password</label>
-        <input class="form-control" placeholder="Password" type="password" name="password" id="password">
+        <input class="form-control {{$errors->first('password') ? "is-invalid" : ""}}" placeholder="Password" type="password" name="password" id="password">
+        <div class="invalid-feedback">
+            {{$errors->first('password')}}
+        </div>
         <br>
+
         <label for="password_confirmation">Password Confirmation</label>
-        <input class="form-control" placeholder="password confirmation" type="password" name="password_confirmation"
+        <input class="form-control {{$errors->first('password_confirmation') ? "is-invalid" : ""}}" placeholder="password confirmation" type="password" name="password_confirmation"
             id="password_confirmation">
+        <div class="invalid-feedback">
+            {{$errors->first('password_confirmation')}}
+        </div>
+
         <br>
         <input class="btn btn-primary" type="submit" value="Save">
     </form>
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -20,11 +20,14 @@ Edit User
         @csrf
 
         <label for="name">Name</label>
-        <input type="text" class="form-control" placeholder="Full Name" name="name" id="name" value="{{$user->name}}">
+        <input type="text" class="form-control {{$errors->first('name') ? "is-invalid" : ""}}" placeholder="Full Name" name="name" id="name" value="{{old('name') ? old('name') : $user->name}}">
+        <div class="invalid-feedback">
+            {{$errors->first('name')}}
+        </div>
         <br>
+        
         <label for="username">Username</label>
-        <input class="form-control" type="text" placeholder="username" name="username" id="username"
-            value="{{$user->username}}" disabled>
+        <input class="form-control" type="text" placeholder="username" name="username" id="username" value="{{$user->username}}" disabled>
         <br>
 
         <label for="status">Status</label>
@@ -32,30 +35,39 @@ Edit User
         <input {{$user->status == "ACTIVE" ? "checked" : ""}} value="ACTIVE" type="radio" class="form-control"
             id="active" name="status">
         <label for="active">Active</label>
-        <input {{$user->status == "INACTIVE" ? "checked" : ""}} value="INACTIVE" type="radio" class="form-control"
-            id="inactive" name="status">
+        <input {{$user->status == "INACTIVE" ? "checked" : ""}} value="INACTIVE" type="radio" class="form-control" id="inactive" name="status">
         <label for="inactive">Inactive</label>
         <br><br>
 
         <label for="roles">Roles</label>
         <br>
-        <input type="checkbox" {{in_array("ADMIN", json_decode($user->roles)) ? "checked" : ""}} name="roles[]"
-            id="ADMIN" value="ADMIN">
+        <input type="checkbox" {{in_array("ADMIN", json_decode($user->roles)) ? "checked" : ""}} name="roles[]" class="form-control {{$errors->first('roles') ? "is-invalid" : ""}}" id="ADMIN" value="ADMIN">
         <label for="ADMIN">Administrator</label>
-        <input type="checkbox" {{in_array("STAFF", json_decode($user->roles)) ? "checked" : ""}} name="roles[]"
-            id="STAFF" value="STAFF">
+        <input type="checkbox" {{in_array("STAFF", json_decode($user->roles)) ? "checked" : ""}} name="roles[]" id="STAFF" value="STAFF" class="form-control {{$errors->first('roles') ? "is-invalid" : ""}}">
         <label for="STAFF">Staff</label>
-        <input type="checkbox" {{in_array("CUSTOMER", json_decode($user->roles)) ? "checked" : ""}} name="roles[]"
-            id="CUSTOMER" value="CUSTOMER">
+        <input type="checkbox" {{in_array("CUSTOMER", json_decode($user->roles)) ? "checked" : ""}} name="roles[]" id="CUSTOMER" class="form-control {{$errors->first('roles') ? "is-invalid" : ""}}" value="CUSTOMER">
         <label for="CUSTOMER">Customer</label>
+        <div class="invalid-feedback">
+            {{$errors->first('roles')}}
+        </div>
+
+
         <br><br>
         <label for="phone">Phone Number</label>
         <br>
-        <input type="text" name="phone" class="form-control" value="{{$user->phone}}">
+        <input type="text" name="phone" class="form-control {{$errors->first('phone') ? "is-invalid" : ""}}" value="{{old('phone') ? old('phone') : $user->phone}}">
+        <div class="invalid-feedback">
+            {{$errors->first('phone')}}
+        </div>
+
         <br>
         <label for="address">Address</label>
-        <textarea name="address" id="address" class="form-control">{{$user->address}}</textarea>
+        <textarea name="address" id="address" class="form-control {{$errors->first('address') ? "is-invalid" : ""}}">{{old('address') ? old('address') : $user->address}}</textarea>
+        <div class="invalid-feedback">
+            {{$errors->first('address')}}
+        </div>
         <br>
+
         <label for="avatar">Avatar</label>
         <br>
         Current avatar: <br>
@@ -70,10 +82,13 @@ Edit User
         <small class="text-muted">
             Kosongkan jika tidak ingin mengubah avatar
         </small>
-        <hr class="my-3">
+        <hr class="my-4">
+
         <label for="email">Email</label>
-        <input class="form-control" placeholder="user@mail.com" type="text" name="email" id="email"
-            value="{{$user->email}}" disabled>
+        <input class="form-control {{$errors->first('email') ? "is-invalid" : ""}}" placeholder="user@mail.com" type="text" name="email" id="email" value="{{$user->email}}" disabled>
+        <div class="invalid-feedback">
+            {{$errors->first('email')}}
+        </div>
         <br>
 
         <input class="btn btn-primary" type="submit" value="Save">
